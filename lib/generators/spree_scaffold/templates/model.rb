019f3adffec5c67ccbe92d888d6b4ae5f2a511e7
Brady Somerville <brady.somerville@gmail.com>
2015-05-05 10:15:53 -0500

    Set root of url/path for images/files to system/

--- lib/generators/spree_scaffold/templates/model.rb
+++ lib/generators/spree_scaffold/templates/model.rb
@@ -14,15 +14,15 @@ module Spree
     has_attached_file :<%= attribute.name %>,
                       styles: { large: '600x600>' },
                       default_style: :large,
-                      url: '/spree/<%= plural_name %>/:id/:style/:basename.:extension',
-                      path: ':rails_root/public/spree/<%= plural_name %>/:id/:style/:basename.:extension',
+                      url: '/system/spree/<%= plural_name %>/:id/:style/:basename.:extension',
+                      path: ':rails_root/public/system/spree/<%= plural_name %>/:id/:style/:basename.:extension',
                       convert_options: { all: '-strip -auto-orient -colorspace sRGB' }
 
     validates_attachment :<%= attribute.name %>, content_type: { content_type: /\Aimage\/.*\Z/ }
 <% elsif attribute.type == :file -%>
     has_attached_file :<%= attribute.name %>,
-                      url: '/spree/<%= plural_name %>/:id/:basename.:extension',
-                      path: ':rails_root/public/spree/<%= plural_name %>/:id/:basename.:extension'
+                      url: '/system/spree/<%= plural_name %>/:id/:basename.:extension',
+                      path: ':rails_root/public/system/spree/<%= plural_name %>/:id/:basename.:extension'
 
 <% end -%>
 <% end -%>
