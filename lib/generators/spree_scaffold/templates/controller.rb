2874f1aaf1d20e29b6816325358329308bf927dc
Brady Somerville <brady.somerville@gmail.com>
2015-05-05 10:15:20 -0500

    Add simple search filter to controller/index template

--- lib/generators/spree_scaffold/templates/controller.rb
+++ lib/generators/spree_scaffold/templates/controller.rb
@@ -2,7 +2,10 @@ module Spree
   module Admin
     class <%= class_name.pluralize %>Controller < ResourceController
       def index
-        @<%= plural_name %> = Spree::<%= class_name %>.page(params[:page] || 1).per(50)
+        @search = @<%= plural_name %>.ransack(params[:q])
+        @<%= plural_name %> = @search.result.
+          page(params[:page]).
+          per(params[:per_page])
       end
     end
   end
